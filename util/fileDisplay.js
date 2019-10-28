32caa9fa8a14b1d298bdee6267d3142612e093ff
Howard Tang <2379034@qq.com>
2019-10-28 17:42:41 +0800

    Update fileDisplay.js

--- util/fileDisplay.js
+++ util/fileDisplay.js
@@ -1,25 +1,20 @@
 var fs = require('fs');
-var path = require('path'); //解析需要遍历的文件夹 
+var path = require('path'); //解析需要遍历的文件夹
 //调用文件遍历方法
 //文件遍历方法
 async function fileDisplay(filePath) {
     //根据文件路径读取文件，返回文件列表
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         let templateList = {};
-        fs.readdir(filePath, async function(err, files) {
-            if (err) {
-                console.warn(err);
-            } else {
-                //遍历读取到的文件列表
-                for (let i = 0; i < files.length; i++) {
-                    var filedir = path.join(filePath, files[i]);
-                    let a = await fs.readdirSync(filedir);
-                    templateList[files[i]] = a;
-                }
 
-                resolve([files, templateList]);
-            }
+        const files = await fs.readdirSync(filePath);
+        await files.map(async file => {
+            var filedir = path.join(filePath, file);
+            let a = await fs.readdirSync(filedir);
+            templateList[file] = a;
         });
+
+        resolve([files, templateList]);
     });
 }
 module.exports = {
